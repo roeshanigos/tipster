e074a5ba90360597950a5964b92ae7bafb580818
Roesha Nigos <roeshanigos@fb.com>
2018-06-26 13:19:23 -0700

    Initial Commit

---
+++ Tipster/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Tipster
+//
+//  Created by Roesha Nigos on 6/26/18.
+//  Copyright © 2018 Roesha Nigos. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
